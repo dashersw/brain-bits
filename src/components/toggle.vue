d6f56a393c8d79a7f416a7515e4a241db4bb46f3
Armagan Amcalar <armagan@amcalar.com>
2017-09-08 20:26:21 +0200

    Implement custom toggle component

---
+++ src/components/toggle.vue
@@ -0,0 +1,88 @@
+<script>
+export default {
+    name: 'toggle',
+    model: { prop: 'checked' },
+    props: {
+        label: '',
+        checked: {
+            type: Boolean,
+            default: false
+        },
+    }
+}
+</script>
+
+<template lang="jade">
+label.toggle
+    span.label {{label}}
+    input(type="checkbox", :checked.prop="checked", @change="$emit('input', $event.target.checked)")
+    span
+</template>
+
+<style lang="scss">
+// modified from http://callmenick.com/post/css-toggle-switch-examples
+.toggle {
+    font-size: 0.4em;
+    display: inline-block;
+    vertical-align: middle;
+    padding: 0vmin 1vmin;
+    line-height: 5em;
+
+    & .label {
+        float: left;
+        font-size: 2.5em;
+        cursor: pointer;
+        padding-right: 1.5vmin;
+    }
+
+    & input {
+        display: none;
+    }
+
+    & input + span {
+        display: block;
+        position: relative;
+        float: right;
+        cursor: pointer;
+        outline: none;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+        border-radius: 5em;
+        transition:  0.4s;
+        border: 0.5em solid #ddd;
+        padding: .5em;
+        width: 10em;
+        height: 5em;
+        background-color: #fff;
+        border-radius: 5em;
+        transition-duration: 0.35s;
+        transition-property: border-color, background-color;
+    }
+
+    & input + span:after {
+        display: block;
+        position: absolute;
+        content: "";
+        top: 0.6em;
+        // left: 1em;
+        // bottom: 1em;
+        width: 3em;
+        height: 3em;
+        background-color: #dddddd;
+        border-radius: 3em;
+        transition-duration: 0.35s;
+        transition-property: transform, background-color;
+    }
+
+    & input:checked + span {
+        border-color: #8ce196;
+    }
+
+    & input:checked + span:after {
+        transform: translateX(5em);
+        background-color: #8ce196;
+    }
+}
+</style>
