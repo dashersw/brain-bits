02aca29a72f76eea9981485c958dd05657b39871
Armagan Amcalar <armagan@amcalar.com>
2017-09-17 21:39:22 +0200

    Update simulator to match the real headset and implement data replays from actual recordings

--- src/models/emotiv/simulator.js
+++ src/models/emotiv/simulator.js
@@ -1,17 +1,27 @@
 import EventEmitter from 'events';
 import { resolution, channels } from './constants';
 
+let counter = 0;
+
 export default class EmotivSimulator extends EventEmitter {
-    constructor() {
+    constructor(filename) {
         super();
 
         this.rate = resolution;
         this.timeout = null;
+
+        if (filename) {
+            this.data = require(filename).data.electrodes;
+            this.counter = 0;
+            this.mode = EmotivSimulator.Mode.REPLAY;
+        } else {
+            this.mode = EmotivSimulator.Mode.RANDOM;
+        }
     }
 
     start() {
         this.timeout = setTimeout(() => {
-            this.emit('data', generateData(), Date.now());
+            this.emit('data', this.generateData(), Date.now());
 
             this.start();
         }, 1000 / this.rate);
@@ -21,15 +31,24 @@ export default class EmotivSimulator extends EventEmitter {
         clearTimeout(this.timeout);
         this.timeout = null;
     }
+
+    generateData() {
+        if (this.mode == EmotivSimulator.Mode.REPLAY) {
+            return this.data[counter++];
+        }
+
+        const electrodes = {};
+        channels.forEach(c => electrodes[c] = this.generateRandomReading());
+
+        return { levels: electrodes };
+    }
+
+    generateRandomReading() {
+        return Math.floor(Math.random() * 16384);
+    }
 }
 
-function generateRandomReading() {
-    return Math.floor(Math.random() * 16384);
-}
-
-function generateData() {
-    const electrodes = {};
-    channels.forEach(c => electrodes[c] = generateRandomReading());
-
-    return electrodes;
-}
+EmotivSimulator.Mode = {
+    RANDOM: 'random',
+    REPLAY: 'replay',
+};
