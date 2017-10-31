dddf165335eac910286b328913af294253fd3fa1
Armagan Amcalar <armagan@amcalar.com>
2017-10-31 12:59:43 +0100

    Revamp grid to increase SNR

--- src/components/grid.vue
+++ src/components/grid.vue
@@ -9,7 +9,7 @@ export default {
 #grid-container
     #grid
         .cell(v-for="cell in matrix.cells" v-bind:key="cell.symbol" v-bind:class="[{ highlighted: cell.highlighted}, cell.highlighted && cell.color]")
-            {{cell.symbol}}
+            .label {{cell.symbol}}
 </template>
 
 <style lang="scss">
@@ -30,55 +30,71 @@ $purple: #8e24aa;
     width: 90vw;
     height: 96vh;
     margin: calc(1vh - 20px) auto;
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    grid-gap: 2vmin;
 
     & .cell {
-        display: inline-block;
-        width: 5vw;
-        height: 6vh;
-        line-height: 6vh;
-        margin: 5vh 5vw;
-        font-size: 4vh;
-        text-align: center;
+        display: flex;
+        align-items: center;
+        justify-content: center;
         opacity: 0.3;
 
         &.highlighted {
             opacity: 1;
-            background: none!important;
-            color: white!important;
 
-            &.red {
+            &.red .label {
                 background: $red;
                 color: $red;
+                box-shadow: inset 0 0 0 2px $red;
             }
 
-            &.orange {
+            &.orange .label {
                 background: $orange;
                 color: $orange;
+                box-shadow: inset 0 0 0 2px $orange;
             }
 
-            &.blue {
+            &.blue .label {
                 background: $blue;
                 color: $blue;
+                box-shadow: inset 0 0 0 2px $blue;
             }
 
-            &.yellow {
+            &.yellow .label {
                 background: $yellow;
                 color: $yellow;
+                box-shadow: inset 0 0 0 2px $yellow;
             }
 
-            &.green {
+            &.green .label {
                 background: $green;
                 color: $green;
+                box-shadow: inset 0 0 0 2px $green;
             }
 
-            &.purple {
+            &.purple .label {
                 background: $purple;
                 color: $purple;
+                box-shadow: inset 0 0 0 2px $purple;
+
             }
         }
 
         &.transparent {
             background: none!important;
+        }
+
+        & .label {
+            height: 6vmin;
+            width: 6vmin;
+            line-height: 6vmin;
+            font-size: 3vmin;
+            color: white!important;
+            border-radius: 50%;
+            background: none !important;
+            // box-shadow: inset 0 0 0 2px rgba(255,255,255,0.3);
+            // box-shadow: none;
         }
     }
 }
